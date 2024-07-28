72b255b467f21ca1d28128fd1e2ddb83df36f022
Dustin Irving <dustin.irving@gmail.com>
2024-07-28 12:25:55 -0400

    style: setup theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,40 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import HealthCarePage from './components/health-care/HealthCarePage';
 import AviationPage from './components/aviation/AviationPage';
 import LandingPage from './components/LandingPage';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        containedPrimary: {
+          backgroundColor: 'var(--emerald-700)',
+          '&:hover': {
+            backgroundColor: 'var(--emerald-800)',
+          },
+        },
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <Router basename="/charlie-team-web/">
-      <div style={{ height: '100%', display: 'flex' }}>
-        <Navbar />
-        <main style={{ flex: 1, minHeight: 0 }}>
-          <Routes>
-            <Route path="*" element={<LandingPage />} />
-            <Route path="health-care/*" element={<HealthCarePage />} />
-            <Route path="aviation/*" element={<AviationPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename="/charlie-team-web/">
+        <div style={{ height: '100%', display: 'flex' }}>
+          <Navbar />
+          <main style={{ flex: 1, minHeight: 0 }}>
+            <Routes>
+              <Route path="*" element={<LandingPage />} />
+              <Route path="health-care/*" element={<HealthCarePage />} />
+              <Route path="aviation/*" element={<AviationPage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -15,7 +15,7 @@ export default function LandingPage() {
         }}
       >
         <Box display="flex" flexDirection="column" flex={1} minHeight={0} p={4} borderRadius="8px">
-          <Typography variant="h4" gutterBottom>
+          <Typography variant="h4" gutterBottom fontWeight="bold">
             Health Care
           </Typography>
           <Box flex={1} minHeight={0}>
@@ -31,7 +31,7 @@ export default function LandingPage() {
             />
           </Box>
           <Box display="flex" flexDirection="column" flex={1} minHeight={0} marginTop="1rem">
-            <Typography variant="body1" paragraph>
+            <Typography variant="body1" paragraph fontSize="18px">
               We provide health care services to those who need it most. Our focus is on treating
               PTSD with innovative psychedelic therapies. These treatments are designed to address
               the root causes of trauma and offer long-lasting relief from symptoms. Our dedicated
@@ -39,19 +39,18 @@ export default function LandingPage() {
               ensuring that each patient receives the attention and support they need to heal and
               recover.
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography variant="body1" paragraph fontSize="18px">
               Our approach includes a combination of therapy, medication, and holistic practices to
               create a comprehensive treatment plan tailored to each individual's needs. By
               leveraging the latest research in psychedelic medicine, we aim to provide cutting-edge
               solutions for mental health challenges.
             </Typography>
-            <Box display="flex" justifyContent="center" margin="2rem">
+            <Box display="flex" justifyContent="center" margin="2rem" height="100%" alignItems="center">
               <Link to="/health-care" style={{ textDecoration: 'none' }}>
                 <Button
                   variant="contained"
                   color="primary"
                   size="large"
-                  sx={{ backgroundColor: '#047857' }}
                 >
                   Learn More
                 </Button>
@@ -69,17 +68,17 @@ export default function LandingPage() {
         />
 
         <Box display="flex" flexDirection="column" flex={1} minHeight={0} p={4} borderRadius="8px">
-          <Typography variant="h4" gutterBottom>
+          <Typography variant="h4" gutterBottom fontWeight="bold">
             Aviation
           </Typography>
           <Box display="flex" flexDirection="column" flex={1} minHeight={0}>
-            <Typography variant="body1" paragraph>
+            <Typography variant="body1" paragraph fontSize="18px">
               We provide aviation services to those who need it most. Our services encompass support
               for the oil, gas, energy, mining, film, defense, and security sectors using advanced
               drone technology. We offer a range of aerial solutions, including surveying, mapping,
               inspections, and surveillance, to enhance operational efficiency and safety.
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography variant="body1" paragraph fontSize="18px">
               Our state-of-the-art drones are equipped with high-resolution cameras and sensors,
               enabling precise data collection and real-time analysis. Whether it's for pipeline
               inspections, geological surveys, or cinematic filming, our drone services are designed
@@ -87,13 +86,12 @@ export default function LandingPage() {
               ensure that all operations are conducted safely and in compliance with regulatory
               standards.
             </Typography>
-            <Box display="flex" justifyContent="center" margin="2rem">
+            <Box display="flex" height="100%" justifyContent="center" margin="2rem" alignItems="center">
               <Link to="/aviation" style={{ textDecoration: 'none' }}>
                 <Button
                   variant="contained"
                   color="primary"
                   size="large"
-                  sx={{ backgroundColor: '#047857' }}
                 >
                   Learn More
                 </Button>
